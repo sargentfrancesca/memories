aa905d06c0ec47b4e3736e46b83d7d5d708a21b9
Francesca Sargent <f@chez.io>
2018-02-03 18:48:42 +0000

    Adding sysarg comment to run file

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from memories import CardReader, Screen, Memories
 
 # Initiate card reader
+# I don't know if 'ABCDEF0123' is just a placeholder or will change,
+# you could add it as a sysarg, or an optional sysarg with a fallback
 card_reader = CardReader('ABCDEF0123')
 screen = Screen()
 memory = Memories(card_reader, screen)
